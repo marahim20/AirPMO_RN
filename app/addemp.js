e4d4f3cb09ce6efaeca4af88bda6d818b68e1d20
marahim20 <minhajabdulrahim@gmail.com>
2023-03-25 13:05:59 +0530

    UI 10%

--- app/addemp.js
+++ app/addemp.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import { Icon } from "@rneui/themed";
 
 const addemp = () => {
   const [loading, setLoading] = useState(true);
@@ -16,17 +17,29 @@ const addemp = () => {
     fetcher();
   }, []);
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack.Screen />
-      {loading ? (
-        <View>
-          <Text style={{ flex: 1 }}>Loading...</Text>
-        </View>
-      ) : (
-        <View>
-          <Text style={{ flex: 1 }}>{projectName}</Text>
-        </View>
-      )}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <Stack.Screen
+        options={{
+          headerTitle: "Job Card Details",
+          headerTitleStyle: {
+            color: "#fff",
+            fontSize: 20,
+          },
+          headerLeft: () => (
+            <Icon
+              name="arrow-left"
+              size={20}
+              color="#fff"
+              style={{ marginLeft: 10 }}
+            />
+          ),
+          headerStyle: {
+            backgroundColor: "#006a4e",
+            borderBottomRightRadius: 20,
+          },
+          headerTintColor: "#fff",
+        }}
+      />
     </SafeAreaView>
   );
 };
